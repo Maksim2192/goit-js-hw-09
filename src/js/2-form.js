6b7f33f83d2c74938c8bfe62131822dc4b81450f
Maksim_Regeda <maxymregeda669@gmail.com>
2025-12-04 02:00:26 +0200

    Home Work9

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = {
+  email: "",
+  message: "",
+};
+
+const form = document.querySelector(".feedback-form");
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", (e) => {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (formData.email === "" || formData.message === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData = { email: "", message: "" };
+});
